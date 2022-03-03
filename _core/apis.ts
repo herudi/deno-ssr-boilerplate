6f975a640b9ff860c132397df7d2357c0ad3ec3f
herudi <herudi7@gmail.com>
2022-03-03 14:43:30 +0700

    merge dev2

--- _core/apis.ts
+++ _core/apis.ts
@@ -1,18 +1,17 @@
-
-import { Router, Handler } from "https://deno.land/x/nhttp@1.1.10/mod.ts";
+import { Handler, Router } from "https://deno.land/x/nhttp@1.1.10/mod.ts";
 import { RequestEvent } from "./deps/types.ts";
 import $0 from "../src/pages/api/about.ts";
 import $1 from "../src/pages/api/blog/index.ts";
 import $2 from "../src/pages/api/blog/[title].ts";
 const api = new Router<RequestEvent>();
 const map = {} as Record<string, Handler<RequestEvent>>;
-  
-map['/api/about.ts'] = $0;
-api.any('/about', $0);
-  
-map['/api/blog/index.ts'] = $1;
-api.any('/blog', $1);
-  
-map['/api/blog/[title].ts'] = $2;
-api.any('/blog/:title', $2);
+
+map["/api/about.ts"] = $0;
+api.any("/about", $0);
+
+map["/api/blog/index.ts"] = $1;
+api.any("/blog", $1);
+
+map["/api/blog/[title].ts"] = $2;
+api.any("/blog/:title", $2);
 export default { api, map };
